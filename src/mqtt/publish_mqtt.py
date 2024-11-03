4a21a93981b465d529e50c901647484f08bed928
ertanhakmet <22003312@hope.ac.uk>
2024-11-03 07:33:30 +0000

    MQTT protocol
    Created python scripts to simulate data and created another python script for mqtt protocol

---
+++ src/mqtt/publish_mqtt.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+import paho.mqtt.client as mqtt
+import time
+from src.simulation.simulation_data import simulate_temp, simulate_hum
+
+# Set up MQTT Broker Details
+broker = "localhost"
+port = 1883
+topic = "smart_home/sensors"
+
+# Create a client instance for MQTT
+client = mqtt.Client()
+client.connect(broker, port)
+
+if __name__ == "__main__":
+    while True:
+        # simulate data
+        temp = simulate_temp()
+        hum = simulate_hum()
+
+        # create a message in JSON format
+        message = f'{{"temperature": {temp}, "humidity": {hum}}}'
+
+        # publish the message to the MQTT topic
+        client.publish(topic, message)
+        print(f"Published: {message} to topic: {topic}")
+
+        time.sleep(5)
